67bf99cd7988a62e7a592d28f6a1371cb2a0e0ef
liujunyu@bailuntec.com <liujunyu@bailuntec.com>
2019-12-11 17:06:54 +0800

    update

--- util/pub.py
+++ util/pub.py
@@ -6,7 +6,7 @@ from .log import logger
 async def pub_to_nsq(address, topic, msg, timeout=60):
     url = urlunparse(ParseResult(scheme='http', netloc='address', path='/pub', params='',
                                  query=urlencode({'topic': topic}), fragment=''))
-    async with ClientSession(timeout=ClientTimeout(total=timeout) as session:
+    async with ClientSession(timeout=ClientTimeout(total=timeout)) as session:
         async with session.request("POST", url, data=msg) as resp:
             if resp.status != 200:
                 logger.error("[pub to nsq error] topic: {}".format(topic))
